d313668590ddd5a9d8ae4f1c6bd099a13c1446d3
Christo <christo@cryptodrunks.net>
2018-11-14 22:45:29 +0100

    added youtube channels pipeline

---
+++ bin/youtube-channels-historic.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+SPREADSHEET_IDS="./queries/spreadsheet-ids.txt"
+
+DATE=$(date +%Y-%m-%d)
+COUNTER=0
+QUERY_COUNT="$(wc -l < "$SPREADSHEET_IDS")"
+
+doit() {
+  "$(npm bin)"/sugarcube \
+              -c pipelines/youtube_channels.json \
+              -Q sheets_query:HistoricScrapes \
+              --google.spreadsheet_id "$1" \
+              --google.to_spreadsheet_id "$1" \
+              --google.to_sheet DailyScrapes \
+              -d
+}
+
+echo "Starting historic scrape of youtube channels."
+
+while IFS="" read -r ID
+do
+  if [ $((COUNTER % 10)) -eq 0 ] && [ "$COUNTER" -gt 0 ]
+  then
+    echo "Processed $COUNTER queries"
+  fi
+
+  doit "$ID" | tee -a ./logs/historic-youtube-channels-"$ID"-"$DATE".log
+
+  if [ "$QUERY_COUNT" -eq $((COUNTER + 1)) ]
+  then
+    exit 0
+  fi
+
+  WAIT_TIME=$(((RANDOM % 10)  + 10))
+  echo "Sleeping for $WAIT_TIME seconds."
+  sleep $WAIT_TIME
+  COUNTER=$((COUNTER+1))
+done < "$SPREADSHEET_IDS"
