76e0343870b88d988225924947e0d950811ab03a
Christo <christo@cryptodrunks.net>
2018-11-15 14:57:32 +0100

    added twitter feeds pipeline

---
+++ bin/twitter-feeds.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+SPREADSHEET_IDS="./queries/spreadsheet-ids.txt"
+
+DATE=$(date +%Y-%m-%d)
+COUNTER=0
+QUERY_COUNT="$(wc -l < "$SPREADSHEET_IDS")"
+
+doit() {
+  "$(npm bin)"/sugarcube \
+              -c pipelines/twitter_feeds.json \
+              -Q sheets_query:TwitterFeeds \
+              --google.spreadsheet_id "$1" \
+              -d
+}
+
+echo "Starting the scrape of Twitter feeds."
+
+while IFS="" read -r ID
+do
+  if [ $((COUNTER % 10)) -eq 0 ] && [ "$COUNTER" -gt 0 ]
+  then
+    echo "Processed $COUNTER queries"
+  fi
+
+  doit "$ID" | tee -a ./logs/twitter-feeds-"$ID"-"$DATE".log
+
+  if [ "$QUERY_COUNT" -eq $((COUNTER + 1)) ]
+  then
+    exit 0
+  fi
+
+  WAIT_TIME=$(((RANDOM % 10)  + 10))
+  echo "Sleeping for $WAIT_TIME seconds."
+  sleep $WAIT_TIME
+  COUNTER=$((COUNTER+1))
+done < "$SPREADSHEET_IDS"
