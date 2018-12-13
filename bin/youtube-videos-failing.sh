d7590a3cc9e71cc3be258c37abed9140d2e647c0
Christo <christo@cryptodrunks.net>
2018-12-13 11:52:57 +0100

    feat: allow to check for failing youtube videos

---
+++ bin/youtube-videos-failing.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DATE=$(date +%Y-%m-%d)
+
+export NODE_OPTIONS=--max_old_space_size=16384
+
+doit() {
+  "$(npm bin)"/sugarcube \
+              -c pipelines/check_failing_videos.json \
+              -q queries/mail-recipients.json \ \
+              -d
+}
+
+echo "Starting a check for failing youtube videos."
+
+doit | tee -a ./logs/failing-videos-"$DATE".log
