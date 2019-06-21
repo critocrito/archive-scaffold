b776720cdac8df9b9a1583c1b3f8ba403f070a0f
Christo <christo@cryptodrunks.net>
2019-06-21 10:57:43 +0200

    fix: improve handling of input data for website data

--- src/clj/archive/scripts/website_data.clj
+++ src/clj/archive/scripts/website_data.clj
@@ -1,7 +1,9 @@
 (ns archive.scripts.website-data
   (:require [clojure.java.io :as io]
             [clojure.string :refer [trim]]
+            [clojure.edn :as edn]
             [clj-time.format :as f]
+            [me.raynes.fs :as fs]
             [archive.core :as core]
             [archive.elastic :as elastic]))
 
@@ -20,26 +22,33 @@
 
 (defn maybe-href
   [x]
-  (if (nil? x)
-    x
-    (str "https://cube.syrianarchive.org/syrian-archive/files/" (.getName (io/file x)))))
+  (let [project-dir (.getName fs/*cwd*)]
+    (if (nil? x)
+      x
+      (str "https://cube.syrianarchive.org/" project-dir "/files/" (.getName (io/file x))))))
 
 (defn website-observation
   [{:keys [$sc_id_hash cid]}]
-  (let [date-string (str (:incident_date cid) " " (:incident_time cid))
-        date-formatter (f/formatter "yyyy-MM-dd HH:mm:ss")]
+  (let [date-formatter (f/formatter "yyyy-MM-dd HH:mm:ss")
+        incident-date (if (nil? (:incident_time cid))
+                        (:incident_date cid)
+                        (f/unparse (f/formatters :date-time-no-ms)
+                                   (f/parse date-formatter (str (:incident_date cid) " " (:incident_time cid)))))]
     {:id $sc_id_hash
      :annotations
      {:online_title_en (maybe-trim (:online_title_en cid))
       :online_title_ar (maybe-trim (:online_title_ar cid))
       :summary_en (maybe-trim (:summary_en cid))
       :summary_ar (maybe-trim (:summary_ar cid))
-      :incident_date_time (f/unparse (f/formatters :date-time-no-ms)
-                                     (f/parse date-formatter date-string))
+      :incident_date_time incident-date
       :location_info
       {:location (maybe-trim (:location cid))
-       :lat (:latitude cid)
-       :lon (:longitude cid)}}
+       :lat (if (string? (:latitude cid))
+              (edn/read-string (:latitude cid))
+              (:latitude cid))
+       :lon (if (string? (:longitude cid))
+              (edn/read-string (:longitude cid))
+              (:longitude cid))}}
      :type_of_violation (:type_of_violation cid)
      :online_link (:online_link cid)
      :href (maybe-href (:filename cid))
