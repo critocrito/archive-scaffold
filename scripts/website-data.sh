e02f0463dca627aa2aed1d5f79859b8fcd56770a
Christo <christo@cryptodrunks.net>
2019-08-05 14:47:24 +0200

    feat: validate missing locations when generating the website data

--- scripts/website-data.sh
+++ scripts/website-data.sh
@@ -9,4 +9,12 @@ mkdir -p "$PROJECT_DIR"
 
 clj -A:website-data > "$OBSERVATIONS"
 
+COUNT_MISSING_LOCATIONS=$(./scripts/verify-website-data.sh "missing-location" "$OBSERVATIONS" | tail -n+2 | grep . -c)
+
+if [ "$COUNT_MISSING_LOCATIONS" != "0" ]
+then
+  echo "Validation error: Found $COUNT_MISSING_LOCATIONS observations with missing location in $OBSERVATIONS."
+  exit 1
+fi
+
 ln -sf "$OBSERVATIONS" "$PROJECT_DIR/observations.json"
