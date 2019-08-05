6260eb645d6af4009afceab0df691f6a7e5cb9ea
Christo <christo@cryptodrunks.net>
2019-08-05 15:42:21 +0200

    feat: check video links of the website data

--- scripts/website-data.sh
+++ scripts/website-data.sh
@@ -10,10 +10,17 @@ mkdir -p "$PROJECT_DIR"
 clj -A:website-data > "$OBSERVATIONS"
 
 COUNT_MISSING_LOCATIONS=$(./scripts/verify-website-data.sh "missing-location" "$OBSERVATIONS" | tail -n+2 | grep . -c)
+COUNT_FAILING_VIDEO_URLS=$(./scripts/verify-website-data.sh "check-video-links" "$OBSERVATIONS" | tail -n+2 | grep . -c)
 
 if [ "$COUNT_MISSING_LOCATIONS" != "0" ]
 then
   echo "Validation error: Found $COUNT_MISSING_LOCATIONS observations with missing location in $OBSERVATIONS."
+  exit 1
+fi
+
+if [ "$COUNT_FAILING_VIDEO_URLS" != "0" ]
+then
+  echo "Validation error: Found $COUNT_FAILING_VIDEO_URLS video URLS don't exist in $OBSERVATIONS."
   exit 1
 fi
 
--- src/clj/archive/scripts/verify_website_data.clj
+++ src/clj/archive/scripts/verify_website_data.clj
@@ -1,5 +1,6 @@
 (ns archive.scripts.verify-website-data
-  (:require [archive.core :as core]))
+  (:require [org.httpkit.client :as http]
+            [archive.core :as core]))
 
 (defn valid-location?
   [{:keys [location] :as data}]
@@ -17,6 +18,7 @@
                  :summary (:summary o)
                  :lang (:lang o)
                  :incident_date_time (:incident_date_time o)
+                 :href (:href o)
                  :location_name (:name loc)
                  :location_lat (:lat loc)
                  :location_lng (:lon loc)})) observations))
@@ -33,18 +35,32 @@
          (reduce fn [])
          map-observations)))
 
+(defn link-checking
+  [observations]
+  (->> observations
+       (filter (fn [o]
+                 (if (nil? (:href o))
+                   true
+                   (let [request {:method :head :url (:href o)}
+                         {:keys [error status]} @(http/request request)]
+                     (if (or error (= status 404))
+                       true
+                       false)))))))
+
 (def columns
-  [:id :location_name :location_lat :location_lng :title :summary :lang :incident_date_time])
+  [:id :location_name :location_lat :location_lng :href :title :summary :lang :incident_date_time])
 
 (defn -main
   [& args]
   (let [op (first args)
-        file (second args)
-        data (core/read-json "observations2.json")
-        observations (:data data)]
+        file (second args)]
     (cond
       (and (= op "missing-location")
            (not (nil? file))) (core/print-csv columns (->> (core/read-json file)
                                                            :data
                                                            missing-location))
+      (and (= op "check-video-links")
+           (not (nil? file))) (core/print-csv columns (->> (core/read-json file)
+                                                           :data
+                                                           link-checking))
       :else (println op "unknown"))))
